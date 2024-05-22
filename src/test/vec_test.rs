721e50a3c1c96af47b6dcb1c2b496e32e5522309
Hakan Vardar <hakovardo123@gmail.com>
2024-05-22 12:56:01 +0300

    Vec3 done

--- src/test/vec_test.rs
+++ src/test/vec_test.rs
@@ -5,11 +5,15 @@ fn vec_add() {
     let v1 = Vec3::new(1, 2, 3);
     let v2 = Vec3::new(3, 2, 1);
 
-    let v3 = Vec3::new(1.0, 2.0, 3.0);
-    let v4 = Vec3::new(0.0, 1.0, 2.0);
-
     assert_eq!(Vec3::new(4, 4, 4), v1 + v2);
-    assert_eq!(Vec3::new(1.0, 3.0, 5.0), v3 + v4);
+}
+
+#[test]
+fn vec_add_float() {
+    let v1 = Vec3::new(1.0, 2.0, 3.0);
+    let v2 = Vec3::new(0.0, 1.0, 2.0);
+
+    assert_eq!(Vec3::new(1.0, 3.0, 5.0), v1 + v2);
 }
 
 #[test]
@@ -17,11 +21,15 @@ fn vec_sub() {
     let v1 = Vec3::new(1, 2, 3);
     let v2 = Vec3::new(3, 2, 1);
 
-    let v3 = Vec3::new(1.0, 2.0, 3.0);
-    let v4 = Vec3::new(0.0, 1.0, 2.0);
-
     assert_eq!(Vec3::new(-2, 0, 2), v1 - v2);
-    assert_eq!(Vec3::new(1.0, 1.0, 1.0), v3 - v4);
+}
+
+#[test]
+fn vec_sub_float() {
+    let v1 = Vec3::new(1.0, 2.0, 3.0);
+    let v2 = Vec3::new(0.0, 1.0, 2.0);
+
+    assert_eq!(Vec3::new(1.0, 1.0, 1.0), v1 - v2);
 }
 
 #[test]
@@ -29,11 +37,17 @@ fn vec_mul() {
     let v1 = Vec3::new(1, 2, 3);
     let v2 = Vec3::new(3, 2, 1);
 
-    let v3 = Vec3::new(1.0, 2.0, 3.0);
-    let v4 = Vec3::new(0.0, 1.0, 2.0);
-
     assert_eq!(Vec3::new(3, 4, 3), v1 * v2);
-    assert_eq!(Vec3::new(0.0, 2.0, 6.0), v3 * v4);
+    assert_eq!(Vec3::new(2, 4, 6), v1 * 2);
+}
+
+#[test]
+fn vec_mul_float() {
+    let v1 = Vec3::new(1.0, 2.0, 3.0);
+    let v2 = Vec3::new(0.0, 1.0, 2.0);
+
+    assert_eq!(Vec3::new(0.0, 2.0, 6.0), v1 * v2);
+    assert_eq!(Vec3::new(2.0, 4.0, 6.0), v1 * 2.0);
 }
 
 #[test]
@@ -41,11 +55,15 @@ fn vec_div() {
     let v1 = Vec3::new(2, 3, 4);
     let v2 = Vec3::new(2, 3, 4);
 
-    let v3 = Vec3::new(1.0, 2.0, 3.0);
-    let v4 = Vec3::new(1.0, 1.0, 2.0);
-
     assert_eq!(Vec3::new(1, 1, 1), v1 / v2);
-    assert_eq!(Vec3::new(1.0, 2.0, 3.0 / 2.0), v3 / v4);
+}
+
+#[test]
+fn vec_div_float() {
+    let v1 = Vec3::new(1.0, 2.0, 3.0);
+    let v2 = Vec3::new(1.0, 1.0, 2.0);
+
+    assert_eq!(Vec3::new(1.0, 2.0, 3.0 / 2.0), v1 / v2);
 }
 
 #[test]
@@ -54,11 +72,32 @@ fn vec_div_by_zero() {
     let v1 = Vec3::new(2, 3, 4);
     let v2 = Vec3::new(0, 0, 0);
 
-    let v3 = Vec3::new(1.0, 2.0, 3.0);
-    let v4 = Vec3::new(0.0, 1.0, 2.0);
-
-    let _ = v3 / v4;
     let _ = v1 / v2;
+}
+
+#[test]
+#[should_panic(expected = "Division by zero")]
+fn vec_div_by_zero_float() {
+    let v1 = Vec3::new(1.0, 2.0, 3.0);
+    let v2 = Vec3::new(0.0, 1.0, 2.0);
+
+    let _ = v1 / v2;
+}
+
+#[test]
+#[should_panic(expected = "Division by zero")]
+
+fn vec_div_by_zero2() {
+    let v1 = Vec3::new(2, 3, 4);
+    let _ = v1 / 0;
+}
+
+#[test]
+#[should_panic(expected = "Division by zero")]
+
+fn vec_div_by_zero2_float() {
+    let v1 = Vec3::new(2.0, 3.0, 4.0);
+    let _ = v1 / 0.0;
 }
 
 #[test]
@@ -68,4 +107,100 @@ fn vec_negate() {
 
     assert_eq!(Vec3::new(-2, -3, -4), -v1);
     assert_eq!(Vec3::new(-2.0, -3.0, -4.0), -v2);
+}
+
+#[test]
+fn vec_negate_float() {
+    let v1 = Vec3::new(2.0, 3.0, 4.0);
+
+    assert_eq!(Vec3::new(-2.0, -3.0, -4.0), -v1);
+}
+
+#[test]
+fn vec_index() {
+    let v1 = Vec3::new(2, 3, 5);
+
+    assert_eq!(2, v1[0]);
+    assert_eq!(3, v1[1]);
+    assert_eq!(5, v1[2]);
+}
+
+#[test]
+#[should_panic(expected = "index out of bounds: the len is 3 but the index is 3")]
+fn vec_index_out_of_bounds() {
+    let v1 = Vec3::new(2, 3, 5);
+    let _ = v1[3];
+}
+
+#[test]
+fn vec_dot() {
+    let v1 = Vec3::new(2, 3, 5);
+    let v2 = Vec3::new(2, 4, 10);
+
+    assert_eq!(4 + 12 + 50, v1.dot(v2))
+}
+
+#[test]
+fn vec_dot_float() {
+    let v1 = Vec3::new(2.0, 3.0, 5.0);
+    let v2 = Vec3::new(2.0, 4.0, 10.0);
+
+    assert_eq!(4.0 + 12.0 + 50.0, v1.dot(v2))
+}
+
+#[test]
+fn vec_cross_product() {
+    let v1 = Vec3::new(1, 2, 3);
+    let v2 = Vec3::new(4, 5, 6);
+
+    assert_eq!(Vec3::new(-3, 6, -3), v1.cross(v2))
+}
+
+#[test]
+fn vec_cross_product_float() {
+    let v1 = Vec3::new(1.2, 2.5, 3.2);
+    let v2 = Vec3::new(10.2, 5.3, 1.2);
+
+    assert_eq!(Vec3::new(-13.96, 31.2, -19.14), v1.cross(v2))
+}
+
+#[test]
+fn vec_distance() {
+    let v1 = Vec3::new(1, 0, 5);
+    let v2 = Vec3::new(0, 2, 4);
+
+    assert_eq!(6.0f64.sqrt(), v1.distance(v2))
+}
+
+#[test]
+fn vec_distance_float() {
+    let v1 = Vec3::new(1.0, 0.0, 5.0);
+    let v2 = Vec3::new(0.0, 2.0, 4.0);
+
+    assert_eq!(6.0f64.sqrt(), v1.distance(v2))
+}
+
+#[test]
+fn vec_from_array() {
+    let v1: Vec3<_> = [0, 2, 3].into();
+
+    assert_eq!(Vec3::new(0, 2, 3), v1);
+}
+
+#[test]
+fn vec_from_tuple() {
+    let v1: Vec3<_> = (0, 2, 3).into();
+
+    assert_eq!(Vec3::new(0, 2, 3), v1);
+}
+
+#[test]
+fn vec_to_iter() {
+    let v1 = Vec3::new(1, 2, 3);
+    let mut iter = v1.into_iter();
+
+    assert_eq!(Some(1), iter.next());
+    assert_eq!(Some(2), iter.next());
+    assert_eq!(Some(3), iter.next());
+    assert_eq!(None, iter.next());
 }
--- src/vec.rs
+++ src/vec.rs
@@ -1,4 +1,5 @@
-use std::ops::{Add, Div, Mul, Neg, Sub};
+#![allow(unused)]
+use std::ops::{Add, Div, Index, Mul, Neg, Sub};
 
 macro_rules! impl_number {
     ($($type:ty),+) => {
@@ -7,7 +8,9 @@ macro_rules! impl_number {
                 fn is_zero(self) -> bool {
                     self == 0 as $type
                 }
-
+                fn to_f64(self) -> f64{
+                    self as f64
+                }
             }
         )+
     };
@@ -21,13 +24,15 @@ pub trait Number:
     + Copy
     + PartialEq
     + PartialOrd
+    + Sized
 {
     fn is_zero(self) -> bool;
+    fn to_f64(self) -> f64;
 }
 
 impl_number!(i8, i16, i32, i64, i128, f32, f64, u8, u16, u32, u64, u128);
 
-#[derive(Debug, PartialEq, PartialOrd)]
+#[derive(PartialEq, PartialOrd, Clone, Copy)]
 pub struct Vec3<T>
 where
     T: Number,
@@ -37,12 +42,79 @@ where
     pub z: T,
 }
 
+impl<T> std::fmt::Debug for Vec3<T>
+where
+    T: Number + std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Vec3")
+            .field("x", &self.x)
+            .field("y", &self.y)
+            .field("z", &self.z)
+            .finish()
+    }
+}
+
+impl<T> From<[T; 3]> for Vec3<T>
+where
+    T: Number,
+{
+    fn from(value: [T; 3]) -> Self {
+        Vec3::new(value[0], value[1], value[2])
+    }
+}
+
+impl<T> From<(T, T, T)> for Vec3<T>
+where
+    T: Number,
+{
+    fn from(value: (T, T, T)) -> Self {
+        Vec3::new(value.0, value.1, value.2)
+    }
+}
+
+impl<T> IntoIterator for Vec3<T>
+where
+    T: Number,
+{
+    type Item = T;
+    type IntoIter = std::array::IntoIter<T, 3>;
+    fn into_iter(self) -> Self::IntoIter {
+        [self.x, self.y, self.z].into_iter()
+    }
+}
+
 impl<T> Vec3<T>
 where
     T: Number,
 {
     pub fn new(x: T, y: T, z: T) -> Self {
         Self { x, y, z }
+    }
+
+    pub fn dot(self, rhs: Self) -> T {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+
+    pub fn cross(self, rhs: Self) -> Self {
+        Vec3::new(
+            self.y * rhs.z - self.z * rhs.y,
+            self.z * rhs.x - self.x * rhs.z,
+            self.x * rhs.y - self.y * rhs.x,
+        )
+    }
+
+    pub fn distance(self, rhs: Self) -> f64 {
+        ((self.x - rhs.x).to_f64().powi(2)
+            + (self.y - rhs.y).to_f64().powi(2)
+            + (self.z - rhs.z).to_f64().powi(2))
+        .sqrt()
+    }
+
+    pub fn length(self) -> f64 {
+        (self.x * self.x + self.y + self.y + self.z + self.z)
+            .to_f64()
+            .sqrt()
     }
 }
 
@@ -97,5 +169,47 @@ where
     type Output = Self;
     fn neg(self) -> Self::Output {
         Vec3::new(-self.x, -self.y, -self.z)
+    }
+}
+
+impl<T> Mul<T> for Vec3<T>
+where
+    T: Number,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs)
+    }
+}
+
+impl<T> Div<T> for Vec3<T>
+where
+    T: Number,
+{
+    type Output = Self;
+    fn div(self, rhs: T) -> Self::Output {
+        if rhs.is_zero() {
+            panic!("Division by zero");
+        }
+
+        Vec3::new(self.x / rhs, self.y / rhs, self.z / rhs)
+    }
+}
+
+impl<T> Index<usize> for Vec3<T>
+where
+    T: Number,
+{
+    type Output = T;
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => {
+                panic!("index out of bounds: the len is 3 but the index is {index}");
+            }
+        }
     }
 }
